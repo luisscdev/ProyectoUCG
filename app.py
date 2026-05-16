51e582c6c116a7219b01338d44d645e62a94021e
Luis Suarez <luisscdev@gmail.com>
2026-05-16 09:50:33 -0500

    ok

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 
 st.title("Proyecto Final")
 
 st.image("python_logo.png")
+
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
