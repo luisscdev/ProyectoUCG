b3c5b5d31b4b89fc80a95d771f6ee93a927eb753
Luis Suarez <luisscdev@gmail.com>
2026-05-16 10:06:08 -0500

    ok

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import libreria_funciones as lf
 
-st.title("Proyecto Final")
+st.title("Proyecto final UCG")
 
-st.image("python_logo.png")
+st.sidebar.title("Parámetros")
 
+st.sidebar.image("Python_logo.png")
 
 uploaded_files = st.file_uploader(
     "Upload data", accept_multiple_files=True, type="csv"
@@ -13,7 +14,11 @@ uploaded_files = st.file_uploader(
 for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
     st.write(df)
-    
-    
-cuota= lf.cuota_prestamo(1000,0.10,5,12)
-st.write(cuota)
+
+monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
+interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
+anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
+numero_pagos = st.number_input("Ingrese el número pagos anuales:" , value = 12)
+
+cuota = lf.cuota_prestamo(monto, interes,anios,numero_pagos)
+st.write("Su cuota mensual es: ",cuota)
